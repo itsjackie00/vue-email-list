354681d643185c408b5dba9fed1c0b48169a33de
itsjackie00 <jackiedanso33@gmail.com>
2024-04-08 16:39:00 +0200

    added in methods a function to generate emails, and printed in my dom with created function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 
 //Descrizione:
@@ -8,11 +8,29 @@ const {createApp} = Vue;
 
 // creazione app
 createApp({
-    data(){
+    data() {
         return {
             emails: [],
             apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            numbEmails: 10,
             loading: false
         }
+    },
+    methods: {
+        // funzione per generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+        generateEmails() {
+            this.loading = true;
+            for (let i = 0; i < this.numbEmails; i++) {
+                axios.get(this.apiUrl).then((response) => {
+                    this.emails.push(response.data.response);
+                    this.loading = false;
+                    console.log(this.emails);
+                })
+            }
+        }
+    },
+    created() {
+        this.generateEmails();
+        console.log(this.emails)
     }
-})
+}).mount('#app')
